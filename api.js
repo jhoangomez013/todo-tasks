c831068231dd8d95d4fde31a83bcbcf489b4131e
Jhoan Sebastián Gómez Medina <jsgomez013@gmail.com>
2025-07-30 21:28:26 -0500

    Todo-tasks

---
+++ api.js
@@ -0,0 +1,72 @@
+var mongoose = require("mongoose");
+var express = require("express");
+var router = express.Router();
+var TaskModel = require("./task_schema");
+
+let environment = null;
+
+if (!process.env.ON_RENDER) {
+  console.log("Cargando variables de entorno desde archivo");
+  const env = require("node-env-file");
+  env(__dirname + "/.env");
+}
+
+environment = {
+  DBMONGOUSER: process.env.DBMONGOUSER,
+  DBMONGOPASS: process.env.DBMONGOPASS,
+  DBMONGOSERV: process.env.DBMONGOSERV,
+  DBMONGO: process.env.DBMONGO,
+};
+
+var query =
+  "mongodb+srv://" +
+  environment.DBMONGOUSER +
+  ":" +
+  environment.DBMONGOPASS +
+  "@" +
+  environment.DBMONGOSERV +
+  "/" +
+  environment.DBMONGO +
+  "retryWrites=true&w=majority";
+
+const db = query;
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conexión a la base de datos exitosa");
+  })
+  .catch((err) => {
+    console.error("Error al conectar a la base de datos", err);
+  });
+
+router.post("/create-task", function (req, res) {
+  let task_id = req.body.TaskId;
+  let name = req.body.Name;
+  let deadline = req.body.Deadline;
+
+  let task = {
+    TaskId: task_id,
+    Name: name,
+    Deadline: deadline,
+  };
+
+  var newTask = new TaskModel(task);
+
+  newTask
+    .save()
+    .then((data) => {
+      res.status(200).send("OK\n");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal error\n");
+    });
+});
+
+module.exports = router;
